f2d878cfabbd12616b729b71aea3fa5c3a5a7545
DRISHYAshrivastava <93528843+DRISHYAshrivastava@users.noreply.github.com>
2024-08-19 01:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,31 +29,31 @@ if send_button:
     if numbers_input and message_input:
         numbers_list = numbers_input.split()
         with st.echo():
-        from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
-        from selenium.webdriver.chrome.service import Service
-        from webdriver_manager.chrome import ChromeDriverManager
-        from webdriver_manager.core.os_manager import ChromeType
-
-        @st.cache_resource
-        def get_driver():
-            return webdriver.Chrome(
-                service=Service(
-                    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-                ),
-                options=options,
-            )
-
-        options = Options()
-        options.add_argument("--disable-gpu")
-        options.add_argument("--headless")
+            from selenium import webdriver
+            from selenium.webdriver.chrome.options import Options
+            from selenium.webdriver.chrome.service import Service
+            from webdriver_manager.chrome import ChromeDriverManager
+            from webdriver_manager.core.os_manager import ChromeType
     
-        driver = get_driver()
-        #Open WhatsApp Web
-        link = 'https://web.whatsapp.com'
-        driver.get(link)
-        st.write("Please scan the QR code through your WhatsApp app to log in to WhatsApp Web. You will get 30 seconds to login.")
-        time.sleep(login_time)
+            @st.cache_resource
+            def get_driver():
+                return webdriver.Chrome(
+                    service=Service(
+                        ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+                    ),
+                    options=options,
+                )
+    
+            options = Options()
+            options.add_argument("--disable-gpu")
+            options.add_argument("--headless")
+        
+            driver = get_driver()
+            #Open WhatsApp Web
+            link = 'https://web.whatsapp.com'
+            driver.get(link)
+            st.write("Please scan the QR code through your WhatsApp app to log in to WhatsApp Web. You will get 30 seconds to login.")
+            time.sleep(login_time)
 
         for num in numbers_list:
             num = num.strip()
